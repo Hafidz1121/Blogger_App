498acd7ee29ca2fc7160c00612916c6ab8d73455
Hafidz Fadhillah Febrianto <63035660+Hafidz1121@users.noreply.github.com>
2023-08-28 14:04:26 +0700

    Update Constant

--- app/src/main/java/com/example/blogapp/Constant.java
+++ app/src/main/java/com/example/blogapp/Constant.java
@@ -12,4 +12,5 @@ public class Constant {
     public static final String DELETE_POST = POSTS + "/delete";
     public static final String LIKE_POST = POSTS + "/like";
     public static final String COMMENTS = POSTS + "/comments";
+    public static final String SHARE = POSTS + "/share";
 }
